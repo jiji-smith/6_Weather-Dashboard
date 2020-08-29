d673cb55eab3124a46f654d630ac9cbdf22cb482
Ji Smith <jihyunjung0219@gmail.com>
2020-08-29 18:20:49 -0500

    added the weather icon

--- script.js
+++ script.js
@@ -2,39 +2,52 @@
 
 
 var cityName = "Austin";
-function searchCityWeather (cityName){
 
-$.get("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=40961afd2ab5d2926debb895b0f079e9",function(todaysData){
-    console.log(todaysData);
-    var lat = todaysData.coord.lat;
-    var lon = todaysData.coord.lon;
+function searchCityWeather(cityName) {
 
-    $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=40961afd2ab5d2926debb895b0f079e9`,function(forecastData){
-        console.log(forecastData);
-          // update our todays forecast Card with current data
-        var humidity = forecastData.current.humidity;
-        var temp = forecastData.current.temp;
-        clearWeather();
-          // print to the dom/html all the forecast data
-        $("#current-temp").text("Temperature: " + temp + "F");
-        $("#current-humidity").text("Humidity: " + humidity + "%");
+    $.get("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=40961afd2ab5d2926debb895b0f079e9", function (todaysData) {
+        // console.log(todaysData);
+        var lat = todaysData.coord.lat;
+        var lon = todaysData.coord.lon;
 
-          for (let i = 1; i < 6; i++) {
-            var tomorrowUnix = forecastData.daily[i].dt
-            var p = $("<p id=fiveDayDate>" + moment.unix(tomorrowUnix).format("MM/DD/YYYY") + "</p>")
-            var _5dayTemp = forecastData.daily[i].temp.day
-            var _5dayHumidity = forecastData.daily[i].humidity
+        $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=40961afd2ab5d2926debb895b0f079e9`, function (forecastData) {
+            // update our todays forecast Card with current data
+            console.log(forecastData);
+            var humidity = forecastData.current.humidity;
+            var temp = forecastData.current.temp;
+            var now = moment().format("MM/DD/YYYY");
+            $("#now-date").append(now);
 
-            $("#"+i).append(p);
-            $("#"+i).append("<p id=fiveDayTemp>"+ _5dayTemp +"</p>")
-            $("#"+i).append("<p id=fiveDayHumidity>"+ _5dayHumidity +"</p>")
+            clearWeather();
+            // showHistory();
 
-          }
-    })
-});
+            // print to the dom/html all the forecast data
+            $("#now-date").text("Today is : " + now);
+            $("#current-temp").text("Temperature: " + temp + "F");
+            $("#current-humidity").text("Humidity: " + humidity + "%");
+
+            for (let i = 1; i < 6; i++) {
+                var tomorrowUnix = forecastData.daily[i].dt
+                var p = $("<p id=fiveDayDate>" + moment.unix(tomorrowUnix).format("MM/DD/YYYY") + "</p>")
+                var _5dayTemp = forecastData.daily[i].temp.day
+                var _5dayHumidity = forecastData.daily[i].humidity
+                var icon = forecastData.daily[i].weather[0].icon;
+                console.log(icon);
+                var inconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
+
+
+                $("#" + i).append(p);
+                $("#" + i).append("<img src=" + inconUrl + ">");
+                $("#" + i).append("<p id=fiveDayTemp>" + _5dayTemp + "</p>")
+                $("#" + i).append("<p id=fiveDayHumidity>" + _5dayHumidity + "</p>")
+
+            }
+        })
+    });
 }
 
-function clearWeather(){
+function clearWeather() {
+    $("#now-date").empty();
     $("#current-temp").empty();
     $("#current-humidity").empty();
     $("#fiveDayTemp").empty();
@@ -43,11 +56,20 @@ function clearWeather(){
 
 }
 
+// function showHistory(){
+//     var searchHistory = cityName();
+//     $(".searchHistory").append(searchHistory);
+// }
 
-$("#searchBtn").on("click", function(){
+// I want the previous serach history
+
+
+$("#searchBtn").on("click", function () {
     event.preventDefault();
-
     cityName = $("#cityName").val().trim();
     console.log(cityName)
     searchCityWeather(cityName);
+    localStorage.setItem("cityName",cityName);
+
 })
+
